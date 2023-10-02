38266cd88c5bd6235fbf0ed812f717a77b2e0eba
ppppppsmash <pppppp.work@gmail.com>
2023-10-02 21:34:51 +0900

    add: sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/assets/css/tailwind.css'],
+  css: [
+    '@/assets/css/tailwind.css',
+    '@/assets/scss/main.scss'
+  ],
   build: {
   },
   modules: [
