7fddc004942b5064be713beaafed1ca680bcd506
ppppppsmash <pppppp.work@gmail.com>
2023-10-02 21:18:38 +0900

    add: 初期init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/tailwind.css'],
+  build: {
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    'nuxt-icon',
+    '@nuxtjs/tailwindcss'
+  ],
+  runtimeConfig: {
+    public: {
+      // key
+    }
+  }
 })
