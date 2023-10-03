2c1a2b70b750f181534e7e98e8371411fa6986fa
ppppppsmash <pppppp.work@gmail.com>
2023-10-03 22:48:53 +0900

    add: thanksページ

---
+++ pages/thanks.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="nes-container is-rounded !h-[95vh]">
+    <p class="font-numochi nes-balloon from-left nes-pointer">
+      ご送信いただきありがとうございました！
+    </p>
+    <DotCharacterA />
+  </div>
+</template>
