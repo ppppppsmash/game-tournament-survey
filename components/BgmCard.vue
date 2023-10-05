e2662e477daa040d66b45b8e9044f815c0e73ca6
ppppppsmash <pppppp.work@gmail.com>
2023-10-05 15:06:13 +0900

    add: favicon

--- components/BgmCard.vue
+++ components/BgmCard.vue
@@ -13,7 +13,7 @@ const mutedHandler = () => {
     <div
       class="transition ease-in-out
         delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-      :class="isMuted ? 'animate-bounce' : 'animate-none'"
+      :class="isMuted ? 'animate-none' : 'animate-bounce'"
       >
       <i
         class="nes-icon is-large star"
