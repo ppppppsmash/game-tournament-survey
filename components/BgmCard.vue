732f2b2204112bdb17af09c133bd5ef179d2065d
ppppppsmash <pppppp.work@gmail.com>
2023-10-05 15:42:34 +0900

    fix: audio muted

--- components/BgmCard.vue
+++ components/BgmCard.vue
@@ -14,16 +14,19 @@ const mutedHandler = () => {
       class="transition ease-in-out
         delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
       :class="isMuted ? 'animate-none' : 'animate-bounce'"
+      @click="mutedHandler"
       >
       <i
         class="nes-icon is-large star"
         :class="[isMuted ? 'is-transparent' : '']"
-        @click="mutedHandler"
       />
     <p class="text-center">BGM</p>
     </div>
-    <audio preload="auto" autoplay loop :muted="isMuted">
-      <source src="/audio/bgm.ogg">
-    </audio>
+
+    <div v-if="!isMuted">
+      <audio preload="auto" autoplay loop>
+        <source src="/audio/bgm.ogg">
+      </audio>
+    </div>
   </div>
 </template>
